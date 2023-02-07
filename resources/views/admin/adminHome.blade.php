728946f61ba285f32870cb61cbe959714515db0d
Henry Caldwell <henrycaldwell423@gmail.com>
2023-02-06 20:58:26 -0500

    added simple email toggle

--- resources/views/admin/adminHome.blade.php
+++ resources/views/admin/adminHome.blade.php
@@ -12,19 +12,17 @@
       </div>
     </div>
 
-  </main>
-
-  <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
       <div class="btn-toolbar mb-2 mb-md-0">
         <div class="btn-group me-2">
-          <button type="button" class="btn btn-sm btn-outline-secondary">Notify All</button>
-          <button type="button" class="btn btn-sm btn-outline-secondary">Notify Individual</button>
+          <button id="showall" type="button" class="btn btn-sm btn-outline-secondary">All</button>
+          <button id="showone" type="button" class="btn btn-sm btn-outline-secondary">One</button>
         </div>
       </div>
     </div>
+    
     <form id="emailform" class="needs-validation" novalidate="" action="" method="POST">
-      <h4></h4>
+      <h4>Email Individual</h4>
       <div class="row g-3">
         <div class="col-sm-6">
           <label for="finduser" class="form-label">Find User
@@ -68,9 +66,28 @@
       @csrf <!-- {{ csrf_field() }} -->
     </form>
 
+    <form style="display:none;" id="emailform2" class="needs-validation" novalidate="" action="" method="POST">
+      <h4>Email All</h4>
+      <div class="row g-3">
+        <div class="col-sm-12">
+          <label for="firstName" class="form-label">Message
+          </label>
+          <textarea name="name" type="text" class="form-control" id="" placeholder="" value="" required=""></textarea>
+          <div class="invalid-feedback">
+            Valid first name is required.
+          </div>
+        </div>
+      </div>
+      <hr class="my-4">
+      <button id="sendemail" class="w-25 btn btn-primary btn-lg">Emails: don't press yet</button>
+      @csrf <!-- {{ csrf_field() }} -->
+    </form>
+  </main>
+
+
+
 <!-----------------------scripts----------------------------------------------------------->
   <script>
-
       //-------------button triggers email event-------------------------
       function SendMail() {
         token = document.querySelector('meta[name="csrf-token"]').content;
@@ -122,7 +139,7 @@
       }
       function findEmail() {
         let data = document.forms.emailform;
-        var name = data['userselect'].value;
+        let name = data['userselect'].value;
         if (currently_visible[name] !== undefined) {
             document.getElementById("emailselect").value = currently_visible[name];
         }
@@ -131,5 +148,18 @@
       document.getElementById("userselect").addEventListener("click", findEmail, true);
       document.getElementById("finduser").addEventListener("keyup", DynamicForm, true);
 
+      //-------------------Form Toggling----------------------------------------------
+      function toggleOne(){
+        document.getElementById("emailform").style.display = 'block';
+        document.getElementById("emailform2").style.display = 'none';
+      }
+      document.getElementById("showone").addEventListener("click", toggleOne, true);
+
+      function toggleAll(){
+        document.getElementById("emailform").style.display = 'none';
+        document.getElementById("emailform2").style.display = 'block';
+      }
+      document.getElementById("showall").addEventListener("click", toggleAll, true);
+      
   </script>
 @endsection
