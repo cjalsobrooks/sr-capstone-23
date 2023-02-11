439a9ac367d1fbde340e81b2c330ca917fb93a3d
Hunter Holland <hunt12435@gmail.com>
2023-02-10 23:59:11 -0500

    Update welcome.blade.php
    Got the navbar working after Henry fixed bootstrap and made the buttons work properly.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,20 +3,22 @@
 
     
 
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        
-        <title>Welcome</title>  
-
-        <!-- Fonts -->
-        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        @vite(['resources/js/app.js'])
-        <!-- Styles -->
-
-
-        <!-- Background image-->
-        <style>
+<head>
+  <title>Volunteer Welcome Page</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <style>
+    /* Remove the navbar's default margin-bottom and rounded borders */ 
+    .navbar {
+      margin-bottom: 0;
+      border-radius: 0;
+    }
+  </style>
+  <!-- Background image-->
+  <style>
             body {
                 font-family: 'Nunito', sans-serif;
                 background-image: url('https://images.squarespace-cdn.com/content/v1/615b1c3397012e292b69d5d3/46440191-5388-43e3-a82d-228c522211b8/DSC_4637.jpg?');
@@ -25,68 +27,42 @@
                 background-size: cover;
             }
         </style>
+</head>
+<body>
 
-        <!-- WORK IN PROGRESS
-            <div>
-                @if (Route::has('login'))
-                    <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                        @auth
-                            <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
-                        @else
-                        <div>
-                            <a href="{{ route('login') }}" class="btn" role="button">Log in</a>
-                        </div>
-                            @if (Route::has('register'))
-                                <a href="{{ route('register') }}" class="btn-info">Register</a>
-                            @endif
-                        @endauth
-                    </div>
-                @endif
-            </div>
-        -->
-        
-        <!-- Page-Text -->
-        
-        
-    </head>
+<nav class="navbar navbar-inverse">
 
-    <body class="antialiased">
-    
-        <header>
-            <nav class="navbar navbar-inverse">
-                <div class="container-fluid">
-                    <div class="navbar-header">
-                        <a class="navbar-brand" href="{{ url('https://www.riverbendfestival.com/') }}">
-                        <img src="https://images.squarespace-cdn.com/content/v1/615b1c3397012e292b69d5d3/d34d336a-8005-4100-8a3a-220ffa5d528c/40TH+LOGO+WHITE.png?format=1500w%20%20%22" alt = "Riverbend Logo" style="width:220px;">
-                        </a>
-                    </div>
-
-                    <ul class="nav navbar-nav">
+  <div class="container-fluid">
+    <div class="navbar-header">
+        <a href="{{ url('https://www.riverbendfestival.com/') }}">
+     <img src="https://images.squarespace-cdn.com/content/v1/615b1c3397012e292b69d5d3/d34d336a-8005-4100-8a3a-220ffa5d528c/40TH+LOGO+WHITE.png?format=1500w%20%20%22" alt = "Riverbend Logo" style="width:220px;"></a>
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>     
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+       <ul class="nav navbar-nav navbar-right">
                         @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                    <a class="nav-link" href="{{ route('login') }}"><span class="glyphicon glyphicon-log-in">{{ __(' Login') }}</a>
                                 </li>
                             @endif
 
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link" href="{{ route('register') }}"><span class="glyphicon glyphicon-user">{{ __(' Register') }}</a>
                                 </li>
                             @endif
                     </ul>
                     @endguest
-                </div>
-            </nav>
-        </header>
-        
-        <div class="sqs-block-content">
-            <style>
-                
-            </style>
-            <h1 style="white-space:pre-wrap;text-align: center;vertical-align: middle;font-weight: bold; font-size: 100px; color: #FFFFFF">Welcome Volunteers!</h1>
-        </div>
-        
+    </div>
+  </div>
+  <h1 class=" bg-info"style="text-align: center;vertical-align: middle;font-weight: bold; font-size: 100px; color: #6aa84f;">Welcome Volunteers!</h1>
+</nav>    
 
-    </body>
+</body>
+
 </html>
