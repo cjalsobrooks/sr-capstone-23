980ce00d9041747e4c1653d838f5be88fbce36d7
Caspar <cjalso@DESKTOP-A69CO4K.localdomain>
2023-02-12 22:36:33 -0500

    added some fields to registration page (is ugly but not forever dw)

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -29,7 +29,7 @@
                             <label for="lastname" class="col-md-4 col-form-label text-md-end">{{ __('Last Name') }}</label>
 
                             <div class="col-md-6">
-                                <input id="lastnname" type="text" class="form-control @error('lastname') is-invalid @enderror" name="lastname" value="{{ old('lastname') }}" required autocomplete="name" autofocus>
+                                <input id="lastname" type="text" class="form-control @error('lastname') is-invalid @enderror" name="lastname" value="{{ old('lastname') }}" required autocomplete="name" autofocus>
 
                                 @error('lastname')
                                     <span class="invalid-feedback" role="alert">
@@ -75,8 +75,27 @@
                             </div>
                         </div>
 
-                        <div class="row mb-0">
+                        <div class="row mb-3">
+                            <label for="group-size" class="col-md-4 col-form-label text-md-end">{{ __('Group?:') }}</label>
+                            <div class=col-md-1>
+                                <input name="isgroup" type="checkbox" value="yes">
+                            </div>
+
+                            <div class="col-md-4">
+                                <input id="group-size" type="number" class="form-control" name="group size">
+                                <label for="group-size" class="form-label">How many?</label>
+                            </div>
+                        </div>
+
+                        <div class="row mb-3 ">
                             <div class="col-md-6 offset-md-4">
+                                <a href="register/waiver">Click here to review the waiver.</a> 
+                                <!-- does not at all work, like at all -->
+                            </div>
+                        </div>
+
+                        <div class="row mb-0">
+                            <div class="col-md-6 offset-md-5">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Register') }}
                                 </button>
