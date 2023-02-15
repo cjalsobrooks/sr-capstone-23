6a0ecfa8dbc577984f0a9be4bd5edae97446a562
Henry Caldwell <henrycaldwell423@gmail.com>
2023-02-15 02:15:03 -0500

    WIP new admin options toggle

--- resources/views/admin/editSchedules.blade.php
+++ resources/views/admin/editSchedules.blade.php
@@ -54,33 +54,79 @@
     </div>
   </div>
 
+
   <div style="display:none;" id="togglecreate">
-    <form id="emailform" class="needs-validation" novalidate="" action="" method="POST">
-      <h4>Choose section leader</h4>
-      <div class="row g-3">
-        <div class="col-sm-6">
-          <label for="finduser" class="form-label">Volunteers by last name
-          </label>
-          <input name="finduser" type="text" class="form-control" id="finduser"  required="">
-        </div>
-        <div class="col-sm-6">
-          <label for="userselect" class="form-label">Name</label>
-          <div class="input-group has-validation">
-            <select class="form-control" id="userselect" name="userselect">
-            </select>
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center">
+      <div></div>
+      <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar align-text-bottom" aria-hidden="true"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
+        Options
+      </button>
+      <ul class="dropdown-menu">
+        <li><a class="dropdown-item createoptionstoggle" href="#">Add Section</a></li>
+        <li><a class="dropdown-item createoptionstoggle" href="#">Add Location</a></li>
+        <li><hr class="dropdown-divider createoptionstoggle"></li>
+        <li><a class="dropdown-item createoptionstoggle" href="#">Add Shift</a></li>
+      </ul>
+    </div>
+
+    <div class="createoptions">
+      <h2 class="fw-bold my-3">Add Section</h2>
+      <form id="emailform" class="needs-validation" novalidate="" action="" method="POST">
+        <h4>Choose section leader</h4>
+        <div class="row g-3">
+          <div class="col-sm-6">
+            <label for="finduser" class="form-label">Volunteers by last name
+            </label>
+            <input name="finduser" type="text" class="form-control" id="finduser"  required="">
+          </div>
+          <div class="col-sm-6">
+            <label for="userselect" class="form-label">Name</label>
+            <div class="input-group has-validation">
+              <select class="form-control" id="userselect" name="userselect">
+              </select>
+            </div>
+          </div>
+          <div class="col-sm-6">
+            <h4>Section Name</h4>
+            <input name="finduser" type="text" class="form-control" id="finduser"  required="">
+          </div>
+          <div class="col-sm-12">
+            <h4>Description</h4>
+            <textarea name="name" type="text" class="form-control" id="firstName" placeholder="" value="" required=""></textarea>
           </div>
         </div>
-        <div class="col-sm-6">
-          <h4>Section Name</h4>
-          <input name="finduser" type="text" class="form-control" id="finduser"  required="">
+        @csrf <!-- {{ csrf_field() }} -->
+      </form>
+      <button id="sendemail" type="button" class="btn btn-success mt-4">Submit</button>
+    </div>
+
+
+    <div style="display:none;" class="createoptions">
+      <h2 class="fw-bold my-3">Add Location</h2>
+      <form id="emailform" class="needs-validation" novalidate="" action="" method="POST">
+        <div class="row g-3">
+          
+          <div class="col-sm-6">
+            <h4>Choose section leader</h4>
+            <div class="input-group has-validation">
+              <select class="form-control" id="userselect" name="userselect">
+              </select>
+            </div>
+          </div>
+          <div class="col-sm-6">
+            <h4>Section Name</h4>
+            <input name="finduser" type="text" class="form-control" id="finduser"  required="">
+          </div>
+          <div class="col-sm-12">
+            <h4>Description</h4>
+            <textarea name="name" type="text" class="form-control" id="firstName" placeholder="" value="" required=""></textarea>
+          </div>
         </div>
-        <div class="col-sm-12">
-          <h4>Description</h4>
-          <textarea name="name" type="text" class="form-control" id="firstName" placeholder="" value="" required=""></textarea>
-        </div>
-      </div>
-      @csrf <!-- {{ csrf_field() }} -->
-    </form>
+        @csrf <!-- {{ csrf_field() }} -->
+      </form>
+      <button id="sendemail" type="button" class="btn btn-success mt-4">Submit</button>
+    </div>
   </div>
 
   <div class="row pt-4 toggleedit"> 
@@ -93,5 +139,19 @@
     @vite(['resources/js/editschedules.js'])  
   @endprepend
   <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.4/index.global.min.js'></script>
+  <script>
+      //ternary toggle
+      function toggleDiv() {
+      var div = document.getElementsByClassName("createoptions");
+      for(var i = 0; i < div.length; i++){
+          div[i].style.display = div[i].style.display == "none" ? "block" : "none";
+        }
+      }
+
+      var elements = document.getElementsByClassName("createoptionstoggle");
+      Array.from(elements).forEach(function(element) {
+        element.addEventListener('click', toggleDiv);
+      });
+  </script>
 
 @endsection
