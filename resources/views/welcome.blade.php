2891a6161bb809ff221b12817b6949307f32bb5c
Hunter Holland <hunt12435@gmail.com>
2023-02-21 21:46:25 -0500

    Fixing the welcome page that I never committed...whoops

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,10 +19,14 @@
         <style>
             body {
                 font-family: 'Nunito', sans-serif;
-                background-image: url('https://images.squarespace-cdn.com/content/v1/615b1c3397012e292b69d5d3/46440191-5388-43e3-a82d-228c522211b8/DSC_4637.jpg?');
+                background-image: url('https://images.squarespace-cdn.com/content/v1/615b1c3397012e292b69d5d3/46440191-5388-43e3-a82d-228c522211b8/DSC_4637.jpg?;');
                 background-repeat: no-repeat;
                 background-attachment: fixed;
                 background-size: cover;
+                width: 100vw;
+                height: 100vh;
+                background-position: center, center;
+                 
             }
         </style>
 </head>
@@ -30,7 +34,7 @@
     <header>
         <nav class="navbar navbar-light bg-dark">
         <a class="navbar-brand" href="https://www.riverbendfestival.com/"><img src="https://images.squarespace-cdn.com/content/v1/615b1c3397012e292b69d5d3/d34d336a-8005-4100-8a3a-220ffa5d528c/40TH+LOGO+WHITE.png?format=1500w%20%20%22" alt = "Riverbend Logo" style="width:220px;"></a>
-        <h1 style="white-space:pre-wrap;text-align: center;vertical-align: middle;font-weight: bold; font-size: 100px; color: #8fce00">Welcome Volunteers!</h1>
+        <h1 style="white-space:pre-wrap;text-align: center;vertical-align: middle;font-weight: bold; font-size: 5vw; color: #8fce00">Welcome Volunteers!</h1>
         
             <ul class="nav justify-content-end">  
                 @guest
