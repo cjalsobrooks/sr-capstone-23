7130747efd93ba16a8938e3736b76119c0efed6b
Caspar Alsobrooks <cjalsobrooks@gmail.com>
2023-02-22 11:26:38 -0500

    added card to welcome page and recolor buttons

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,7 +19,9 @@
         <style>
             body {
                 font-family: 'Nunito', sans-serif;
-                background-image: url('https://images.squarespace-cdn.com/content/v1/615b1c3397012e292b69d5d3/46440191-5388-43e3-a82d-228c522211b8/DSC_4637.jpg?;');
+                background-image: 
+                linear-gradient(to bottom, rgba(245, 246, 252, 0.52), rgba(0, 57, 71, 0.73)),
+                url('https://images.squarespace-cdn.com/content/v1/615b1c3397012e292b69d5d3/46440191-5388-43e3-a82d-228c522211b8/DSC_4637.jpg?;');
                 background-repeat: no-repeat;
                 background-attachment: fixed;
                 background-size: cover;
@@ -30,17 +32,17 @@
             }
         </style>
 </head>
-<body>
+<body class="align-items-center">
     <header>
-        <nav class="navbar navbar-light bg-dark">
-        <a class="navbar-brand" href="https://www.riverbendfestival.com/"><img src="https://images.squarespace-cdn.com/content/v1/615b1c3397012e292b69d5d3/d34d336a-8005-4100-8a3a-220ffa5d528c/40TH+LOGO+WHITE.png?format=1500w%20%20%22" alt = "Riverbend Logo" style="width:220px;"></a>
-        <h1 style="white-space:pre-wrap;text-align: center;vertical-align: middle;font-weight: bold; font-size: 5vw; color: #8fce00">Welcome Volunteers!</h1>
+        <nav class="navbar navbar-light bg-dark">            
+            <a class="navbar-brand" href="https://www.riverbendfestival.com/"><img src="https://images.squarespace-cdn.com/content/v1/615b1c3397012e292b69d5d3/d34d336a-8005-4100-8a3a-220ffa5d528c/40TH+LOGO+WHITE.png?format=1500w%20%20%22" alt = "Riverbend Logo" style="width:220px;"></a>
+            <h1 style="white-space:pre-wrap;text-align: left;vertical-align: left;font-weight: bold; font-size: 2vw; color: #FFFFFF">Welcome Volunteers!</h1>                 
         
-            <ul class="nav justify-content-end">  
+            <ul class="nav justify-content-end" style="margin-right: 2.5em">  
                 @guest
                     @if (Route::has('login'))
                         <li class="nav-item">
-                        <a class="btn btn-primary" href="{{ route('login') }}" role="button">Login</a>
+                        <a class="btn btn-warning" href="{{ route('login') }}" role="button" style="margin-right:1em">Login</a>
                         </li>
                     @endif
 
@@ -54,6 +56,24 @@
             </ul>
         </nav>
     </header>
+
+    <div class="container d-flex align-items-center justify-content-center" style="margin-top: 2.5em">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card mx-auto">
+                    <div class="card-body align-items-center">
+                        <h3 class="card-title" style="text-align:center">There would be no Riverbend Festival without volunteers like you.<h1>
+
+                        <div class="d-grid gap-2 col-6 mx-auto">
+                            <a class="btn btn-warning" type="button" href="https://www.riverbendfestival.com">Festival Information</a>
+                            <button class="btn btn-warning" type="button">FAQ/User Guide (TODO)</button>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
         
 
 </body>
