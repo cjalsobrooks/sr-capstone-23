dd68aca6807e1ebf5e9d5162e82a57795c2e193f
NaveenaPriestley <68829047+NaveenaPriestley@users.noreply.github.com>
2023-02-22 19:21:38 -0500

    Update emailSupervisor.blade.php
    Cosmetic change: "Admin" changed to "Volunteer"

--- resources/views/user/emailSupervisor.blade.php
+++ resources/views/user/emailSupervisor.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-      <h2 class="h2"><span class="fw-bold">Admin : </span>{{ Auth::user()->first_name }}</h2>
+      <h2 class="h2"><span class="fw-bold">Volunteer : </span>{{ Auth::user()->first_name }}</h2>
       <div class="btn-toolbar mb-2 mb-md-0">
         <div class="btn-group me-2">
           <button id="showall" type="button" class="btn btn-sm btn-outline-secondary">All</button>
