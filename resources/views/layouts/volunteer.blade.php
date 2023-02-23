31cfc1e6869595c75a3fa349ae3defd10414f442
NaveenaPriestley <68829047+NaveenaPriestley@users.noreply.github.com>
2023-02-22 19:32:27 -0500

    Update volunteer.blade.php
    No search bar

--- resources/views/layouts/volunteer.blade.php
+++ resources/views/layouts/volunteer.blade.php
@@ -22,15 +22,16 @@
     
 </head>
 <body>
-    <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-        <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#">Admin Panel</a>
+<header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+        <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#">Volunteer Panel</a>
         <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-        <input class="form-control form-control-dark w-100 rounded-0 border-0" type="text" placeholder="Search" aria-label="Search">
+        <div class="form-control-dark w-100 rounded-0 border-0"></div>
         <div class="navbar-nav">
           <div class="nav-item text-nowrap">
-            <a class="dropdown-item nav-link px-3" href="{{ route('logout') }}" onclick="event.preventDefault();  document.getElementById('logout-form').submit();">
+        <div class="form-control-dark w-100 rounded-0 border-0"></div>
+            <a style="background-color: rgba(0, 0, 0, 0.25); width: 114.891px; text-align:center;" class="dropdown-item nav-link px-3" href="{{ route('logout') }}" onclick="event.preventDefault();  document.getElementById('logout-form').submit();">
                 {{ __('Sign out') }}
             </a>
          <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
@@ -63,7 +64,7 @@
                   </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/editusers">
+                    <a class="nav-link" href="/#">
                       <span data-feather="shopping-cart" class="align-text-bottom"></span>
                       View Group
                     </a>
