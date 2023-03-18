92229b11a7a7e2e54452c8c9313dc0c25332738e
Caspar <cjalso@BADJARVIS.localdomain>
2023-03-18 16:24:25 -0400

    display calendar user home

--- resources/views/user/userHome.blade.php
+++ resources/views/user/userHome.blade.php
@@ -14,7 +14,9 @@
       </p>
 
       <h2 id="schedule">Your Shift Schedule</h2>
-      <!-- <div id='calendar'></div> -->
+      <div style="" class="editoptions col-12-sm my-4" id="showcalendar">
+        <div style="max-height: 400px;" id="calendarSec"></div>
+      </div>
       <table class="table">
         <thead>
           <tr>
@@ -49,6 +51,41 @@
   </main>
 
   <!-- <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.4/index.global.min.js%27%3E'></script> -->
+  <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.4/index.global.min.js'></script>
+  <script>
+
+      var calendarEl = document.getElementById('calendarSec');
+            
+      var calendar = new FullCalendar.Calendar(calendarEl, {
+        allDaySlot: false,
+        initialView: 'timeGrid',
+        initialDate: '2023-06-02',
+        duration: {days: 3},
+        headerToolbar: {
+          left: '',
+          center: 'title',
+          right: ''
+        },
+          eventClick: function(info) {
+            info.jsEvent.preventDefault(); // don't let the browser navigate
+
+            if (info.event.url) {
+              var xhttp = new XMLHttpRequest();
+                xhttp.open("get", `${info.event.url}`, true);
+                xhttp.setRequestHeader("X-CSRF-TOKEN", token);  
+                xhttp.send();
+                xhttp.onload = function(){
+                  alert(xhttp.response);
+                  calendar.removeAllEvents();
+                }
+            }
+        }
+      });
+
+      document.addEventListener('DOMContentLoaded', function() {
+        calendar.render();
+      });
+  </script>
   @prepend('js')
     @vite(['resources/js/adminhome.js'])   
   @endprepend
