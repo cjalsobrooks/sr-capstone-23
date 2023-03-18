656a8c0a21a0bcd09fa7f9ac893c11d9f74a42b8
Caspar Alsobrooks <cjalsobrooks@gmail.com>
2023-03-18 13:37:54 -0400

    section lead

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,8 +1,18 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Exception;
+use Auth;
+use \Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Volunteer;
+use App\Models\Section;
+use App\Models\Location;
+use App\Models\Shift;
+use App\Models\Roster;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\NotifyUsers;
 
 class UserController extends Controller
 {
@@ -35,7 +45,18 @@ class UserController extends Controller
     }
 
     public function sectionLead() {
-        // actual logic will go here not complete obviously
+        // //actual logic will go here not complete obviously
+        // $sections = DB::table('sections')
+        //     ->join('volunteers', 'sections.volunteer_id', '=', 'volunteers.id')
+        //     ->join('users', 'volunteers.user_id', '=', 'users.id')
+        //     ->select('sections.name', 'sections.description', 'sections.id', 'volunteers.first_name', 'volunteers.last_name', 'volunteers.id as volId', 'users.id as uId')
+        //     ->get();
+
+        //  $sec = Section::where('userId', $cId);
+        // $userSec = [];
+        // foreach($sec as $s) {
+        //     array_push($userSec, $s);
+        // }
         return view('lead.secLeadInfo');
     } 
 }
--- resources/views/lead/secLeadInfo.blade.php
+++ resources/views/lead/secLeadInfo.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-      <h2 class="h2"><span class="fw-bold">Lead: </span>{{ Auth::user()->first_name }} {{ Auth::user()->last_name }}</h2>
-      <h3 class="h3"><span class="fw-bold">Section: </span>section name here</h3>
+      <h2 class="h2"><span class="fw-bold">Section: </span>section name here</h2>
+      <h2 class="h2"><span class="fw-bold">Lead: </span>{{ Auth::user()->first_name }} {{ Auth::user()->last_name }}</h2>      
     </div>
     
     <div class="container mt-5">
